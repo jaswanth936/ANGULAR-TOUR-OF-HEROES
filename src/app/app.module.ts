11e291776ac2b8415447b133279216b478fc2474
jaswanth936 <jaswanth.dwarak@gmail.com>
2017-07-05 18:36:35 -0400

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms"; //<!-- NgModel lives here -->
+import { HttpModule } from "@angular/http";
+import { AppRoutingModule } from "./app-routing.module";
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./services/in-memory-data.service";
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { HeroesComponent } from "./heroes/heroes.component";
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import { HeroService } from "./services/hero.service";
+import { HeroSearchComponent } from "./hero-search/hero-search.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule, // import the FormsModule before binding with [(ngModel)]
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
